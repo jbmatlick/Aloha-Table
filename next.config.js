2460536497884507a11ed633ad23bfc154cd27a7
James Matlick <jbmatlick@gmail.com>
2025-05-09 08:09:26 -1000

    Add www to non-www redirect and security headers

--- next.config.js
+++ next.config.js
@@ -12,6 +12,34 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          }
+        ]
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.salt-and-serenity.com',
+          },
+        ],
+        destination: 'https://salt-and-serenity.com/:path*',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig 
