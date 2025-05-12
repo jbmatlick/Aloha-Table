69b0f427b79ac3944b7cc5478d49a184625762a1
James Matlick <jbmatlick@gmail.com>
2025-05-11 16:29:34 -1000

    fix: update Auth0 config type

--- src/app/api/auth/[...auth0]/config.ts
+++ src/app/api/auth/[...auth0]/config.ts
@@ -1,6 +1,6 @@
-import { Auth0Config } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0';
 
-export const auth0Config: Auth0Config = {
+export const auth0Config = {
   session: {
     rollingDuration: 60 * 15, // 15 minutes
     absoluteDuration: 60 * 60, // 1 hour
